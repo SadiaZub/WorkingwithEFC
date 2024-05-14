40456800c9d8d4c98f5d5d3f8e7b2f0a46188c14
SadiaZub <sadiazubair644@gmail.com>
2024-05-14 12:24:00 +0500

    adding deleting entities

---
+++ DeletingEntities/WorkingWithEFCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using Packt.Shared;
+
+//QueryingCategories();
+
+// FilteredIncludes();
+
+// QueryingProducts();
+
+//QueryingWithLike();
+
+//GetRandomProduct();
+
+
+WriteLine("About to delete all products whose name starts with Bob.");
+Write("Press Enter to continue or any other key to exit: ");
+if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
+{
+  int deleted = DeleteProducts(productNameStartsWith: "Bob");
+  WriteLine($"{deleted} product(s) were deleted.");
+}
+else
+{
+  WriteLine("Delete was canceled.");
+}
