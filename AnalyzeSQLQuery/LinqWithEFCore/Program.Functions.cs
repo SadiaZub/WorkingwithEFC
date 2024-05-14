877aae028d1bbf864d0374361038d1420e060ed1
SadiaZub <sadiazubair644@gmail.com>
2024-05-14 22:04:18 +0500

    new code for chapter

---
+++ AnalyzeSQLQuery/LinqWithEFCore/Program.Functions.cs
@@ -0,0 +1,26 @@
+using Packt.Shared; // Northwind, Category, Product
+using Microsoft.EntityFrameworkCore; // DbSet<T>
+
+partial class Program
+{
+  static void FilterAndSort()
+  {
+    SectionTitle("Filter and sort");
+    using (Northwind db = new())
+    {
+      DbSet<Product> allProducts = db.Products;
+      IQueryable<Product> filteredProducts =
+      allProducts.Where(product => product.UnitPrice < 10M);
+      IOrderedQueryable<Product> sortedAndFilteredProducts =
+      filteredProducts.OrderByDescending(product => product.UnitPrice);
+      WriteLine(sortedAndFilteredProducts.ToQueryString());
+      WriteLine("Products that cost less than $10:");
+      foreach (Product p in sortedAndFilteredProducts)
+      {
+        WriteLine("{0}: {1} costs {2:$#,##0.00}",
+        p.ProductId, p.ProductName, p.UnitPrice);
+      }
+      WriteLine();
+    }
+  }
+}
